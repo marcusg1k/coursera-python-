1ff9afe06cf45559795b1e7e38bc2abdc263e44a
Marcus <marcusgutierrezufc@gmail.com>
2024-05-18 15:15:26 -0500

    basics

--- Python/basics.py
+++ Python/basics.py
@@ -41,3 +41,18 @@ def lucky_number(name):
     
 lucky_number("Marcus")
 lucky_number("Npc")
+
+def calculate(d):
+    q = 3.14
+    z = q * (d ** 2) 
+    print(z) 
+
+calculate(5) 
+
+def circle_area(radius): 
+    pi = 3.14
+    area = pi * (radius ** 2) 
+    print(area) 
+
+circle_area(5) 
+#output should be the same for both 78.5
--- Python/testrun.py
+++ Python/testrun.py
@@ -1,6 +1,14 @@
 def calculate(d):
     q = 3.14
-    z = q * (d ** 2)
+    z = q * (d ** 2) 
     print(z) 
 
 calculate(5) 
+
+def circle_area(radius): 
+    pi = 3.14
+    area = pi * (radius ** 2) 
+    print(area) 
+
+circle_area(5) 
+#output should be the same for both 78.5
