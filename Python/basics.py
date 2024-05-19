8e755d4ac51f88cd8d83ce31361aee6952e25765
Marcus <marcusgutierrezufc@gmail.com>
2024-05-19 17:09:58 -0500

    basics

--- Python/basics.py
+++ Python/basics.py
@@ -243,3 +243,22 @@ print(10*4 > 14+23)
 #prints true because 40 is greater than 37
 print("tall" < "short")
 #prints false because tall is greater than short (unicode values)
+
+
+def translate_error_code(error_code):
+ 
+    if error_code == "401 Unauthorized":
+        translation = "Server received an unauthenticated request"
+ 
+    elif error_code == "404 Not Found":
+        translation = "Requested web page not found on server"
+    elif error_code == "408 Request Timeout":
+        translation = "Server request to close unused connection"
+ 
+    else:
+        translation = "Unknown error code"
+    return translation
+
+print(translate_error_code("404 Not Found"))
+
+# Outputs Requested web page not found on server
