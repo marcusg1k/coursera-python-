a6b907b0ad1f401c807208510756ee6bcc947b1d
Marcus <marcusgutierrezufc@gmail.com>
2024-05-19 15:16:27 -0500

    basics

--- Python/basics.py
+++ Python/basics.py
@@ -228,3 +228,13 @@ def hint_username(username):
         print("Username successfully created.")
 
 hint_username("Marcus")
+
+def hint_username(username):
+    if len(username) < 3:
+        print("Invalid username. Must be at least 3 characters long")
+    else:
+        if len(username) > 15:
+            print("Invalid username. Must be at most 15 characters long")
+        else:
+            print("Valid username")
+hint_username("MarcusDriplordAurelius")
--- Python/testrun.py
+++ Python/testrun.py
@@ -1,7 +1,9 @@
 def hint_username(username):
-    if len(username) < 3: 
-        print("Invalid username. Must be at least 3 characters long.")
+    if len(username) < 3:
+        print("Invalid username. Must be at least 3 characters long")
     else:
-        print("Username successfully created.")
-
-hint_username("Marcus")
+        if len(username) > 15:
+            print("Invalid username. Must be at most 15 characters long")
+        else:
+            print("Valid username")
+hint_username("MarcusDriplordAurelius")
