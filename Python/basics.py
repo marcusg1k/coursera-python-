855db8bd7ebdb42f9910e1646bced52a5ec24763
Marcus <marcusgutierrezufc@gmail.com>
2024-05-19 15:08:30 -0500

    basics

--- Python/basics.py
+++ Python/basics.py
@@ -221,3 +221,10 @@ print(not x)
 today = "Sunday" 
 print(not today == "Monday")
 #returns true bc sunday is NOT equal to monday
+def hint_username(username):
+    if len(username) < 3: 
+        print("Invalid username. Must be at least 3 characters long.")
+    else:
+        print("Username successfully created.")
+
+hint_username("Marcus")
--- Python/testrun.py
+++ Python/testrun.py
@@ -1,4 +1,7 @@
 def hint_username(username):
     if len(username) < 3: 
         print("Invalid username. Must be at least 3 characters long.")
+    else:
+        print("Username successfully created.")
 
+hint_username("Marcus")
