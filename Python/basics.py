2e4a1d7538a2e5c3e1e56fdc99eb3bc13cd187d0
Marcus <marcusgutierrezufc@gmail.com>
2024-05-19 10:57:49 -0500

    basics

--- Python/basics.py
+++ Python/basics.py
@@ -142,3 +142,54 @@ True                # NOT equal to each other. If true, Python
 
 print(9/3 != 3*1)   # In this last example, 9/3 != 3*1 (or 3 != 3)
 False               # is false. So, Python returns a False value.
+
+# The = equals assignment operator is used to assign a value to a 
+# variable.
+
+my_variable = 3*5           # Assigns a value to my_variable      
+print(my_variable)          # Printing the variable returns the 
+15                          # value assigned to the variable.
+
+
+                              
+# The == equality comparison operator checks if the values of the two
+# expressions on either side of the == operator are equivalent to one 
+# another.
+      
+print(my_variable == 3*5)   # Printing the variable returns a Boolean 
+True                        # True or False result. 
+
+# The == operator can check if two strings are equal to each other. 
+# If they are equal, the Python interpreter returns a True result.
+print("a string" == "a string")
+True
+
+
+# In this example, the equality == comparison is between "4 + 5" and
+# 4 + 5. Since the left data type is a string and the right data type
+# is an integer, the two values cannot be equal. So, the comparison
+# returns a False result.
+print("4 + 5" == 4 + 5)
+False
+
+
+# The != operator can check if the two strings are NOT equal to each
+# other. If they are indeed not equal, then Python returns a True result.
+print("rabbit" != "frog")
+True
+
+
+# In this example, the variable event_city has been assigned the string 
+# value "Shanghai". This variable is compared to a static string, 
+# "Shanghai", using the != operator. As, the strings "Shanghai" and 
+# "Shanghai" are the same, the comparison of "Shanghai" != "Shanghai" 
+# is false. Accordingly, Python will return a False result.
+event_city = "Shanghai"
+print(event_city != "Shanghai")
+False
+
+# This last example illustrates the result of trying to compare two
+# items of different data types using the equality == operator. The
+# two items are not equal, so the comparison returns False.
+print("three" == 3)
+False
