ff2d0b2b90a309f2a3136985b6bc9d89b902489e
Marcus <marcusgutierrezufc@gmail.com>
2024-05-20 12:36:50 -0500

    basics

--- Python/basics.py
+++ Python/basics.py
@@ -500,3 +500,19 @@ def sum_of_integers(n):
 print(sum_of_integers(3)) # should print 6
 print(sum_of_integers(4)) # should print 10
 print(sum_of_integers(5)) # should print 15
+
+#fill in blanks for multiplcation table
+
+def multiplication_table(number): 
+    multiplier = 1
+
+    while multiplier <= 10: 
+        result = number * multiplier
+        if result > 25: 
+            break
+        print(str(number) + "x" + str(multiplier) + "=" + str(result)) 
+
+        multiplier += 1
+
+multiplication_table(3) #should print 3x1=3, 3x2=6, 3x3=9, 3x4=12, 3x5=15
+multiplication_table(5)
--- Python/testrun.py
+++ Python/testrun.py
@@ -1,17 +1,15 @@
-#function that takes argument n and returns the sum of the integers
+#fill in blanks for multiplcation table
 
-def sum_of_integers(n):
-    if n < 1:
-        return 0
-    
-    i = 1
-    sum = 0
-    while i <= n: 
-        sum = sum + i
-        i = i + 1
+def multiplication_table(number): 
+    multiplier = 1
 
-    return sum
+    while multiplier <= 10: 
+        result = number * multiplier
+        if result > 25: 
+            break
+        print(str(number) + "x" + str(multiplier) + "=" + str(result)) 
 
-print(sum_of_integers(3)) # should print 6
-print(sum_of_integers(4)) # should print 10
-print(sum_of_integers(5)) # should print 15
+        multiplier += 1
+
+multiplication_table(3) #should print 3x1=3, 3x2=6, 3x3=9, 3x4=12, 3x5=15
+multiplication_table(5)
