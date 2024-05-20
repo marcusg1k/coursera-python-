475ab3ed53dc2aa0ef65e0db1ff0c7d638faf51f
Marcus <marcusgutierrezufc@gmail.com>
2024-05-19 20:30:01 -0500

    conditionals quiz added

--- Python/basics.py
+++ Python/basics.py
@@ -262,3 +262,50 @@ def translate_error_code(error_code):
 print(translate_error_code("404 Not Found"))
 
 # Outputs Requested web page not found on server
+
+#quiz conditionals
+#1) Whats the value of this Python expression: 
+(2**2) == 4
+#2) Complete the script by filling in the missing parts. The function receives a name, then returns a greeting based on whether or not that name is "Taylor".
+def greeting(name):
+  if name == "Taylor":
+    return "Welcome back Taylor!"
+  else:
+    return "Hello there, " + name
+
+print(greeting("Taylor"))
+print(greeting("John"))
+#correct
+#3) What's the output of this code if number equals 10? 
+if number > 11: 
+    print(0)
+elif number != 10:
+    print(1)
+elif number >= 20 or number < 12:
+    print(2)
+else: 
+    print(3)
+    #output is 2
+#4) Is "A dog" smaller or larger than "A mouse"? Is 9999+8888 smaller or larger than 100*100? 
+#Replace the plus sign with a comparison operator in the following code to let Python check it for you and then answer. The result should return True if the correct comparison operator is used. 
+print("A dog" + "A mouse")
+print(9999+8888 + 100*100) 
+#5) 
+def calculate_storage(filesize):
+    block_size = 4096
+    # Use floor division to calculate how many blocks are fully occupied
+    full_blocks = filesize // block_size
+    # Use the modulo operator to check whether there's any remainder
+    partial_block_remainder = filesize % block_size
+    # Depending on whether there's a remainder or not, return
+    # the total number of bytes required to allocate enough blocks
+    # to store your data.
+    if partial_block_remainder > 0:
+        return block_size * (full_blocks +1)
+    return block_size * full_blocks
+print(calculate_storage(1))    # Should be 4096
+print(calculate_storage(4096)) # Should be 4096
+print(calculate_storage(4097)) # Should be 8192
+print(calculate_storage(6000)) # Should be 8192
+#correct
+
