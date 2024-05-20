85d246db4c56e8f37b7e9437dd1587f6edfa6803
Marcus <marcusgutierrezufc@gmail.com>
2024-05-20 03:17:37 -0500

    basics

--- Python/basics.py
+++ Python/basics.py
@@ -385,3 +385,17 @@ print(greater_value(10, 3*5))
 
 print((24 == 5*2) and (24 > 3*5) and (2*6 == 12)) 
 #should output false
+
+#fill in blanks to complete the safe_division function
+
+def safe_division(numerator, denominator): 
+    if denominator == 0: 
+        result = 0
+    else: 
+        result = numerator / denominator
+    return result
+
+print(safe_division(5, 5)) #should print 1.0
+print(safe_division(5, 4)) #should print 1.25
+print(safe_division(5, 0)) #should print 0
+print(safe_division(0, 5)) #should print 0.0
--- Python/testrun.py
+++ Python/testrun.py
@@ -1,5 +1,15 @@
-#What's the value of this python expression? 
+#fill in blanks to complete the safe_division function
 
-print((24 == 5*2) and (24 > 3*5) and (2*6 == 12)) 
-#should output false
+def safe_division(numerator, denominator): 
+    if denominator == 0: 
+        result = 0
+    else: 
+        result = numerator / denominator
+    return result
+
+print(safe_division(5, 5)) #should print 1.0
+print(safe_division(5, 4)) #should print 1.25
+print(safe_division(5, 0)) #should print 0
+print(safe_division(0, 5)) #should print 0.0
+
 
