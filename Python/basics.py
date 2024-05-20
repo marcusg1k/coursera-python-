07e8457fa1c2e4aa80710c0d338f9e8a4ab956c3
Marcus <marcusgutierrezufc@gmail.com>
2024-05-20 02:55:57 -0500

    basics

--- Python/basics.py
+++ Python/basics.py
@@ -318,4 +318,12 @@ print(name + "'s favorite food is " + fav_food + ".")
 print("blue" == "Blue")
 #output is false bc strings are not equal
 
+number = 4
+if number * 4 < 15:
+    print(number / 4)
+elif number < 5: 
+    print(number + 3)
+else:
+    print(number * 2 % 5)
 
+#output should be 7 because 4 < 5 and 4 + 3 = 7
--- Python/testrun.py
+++ Python/testrun.py
@@ -1,1 +1,9 @@
 number = 4
+if number * 4 < 15:
+    print(number / 4)
+elif number < 5: 
+    print(number + 3)
+else:
+    print(number * 2 % 5)
+
+#output should be 7 because 4 < 5 and 4 + 3 = 7
