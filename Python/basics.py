6c04e76b5bf90a458c170a5b1f31dc04027f4535
Marcus <marcusgutierrezufc@gmail.com>
2024-05-20 11:20:42 -0500

    basics

--- Python/basics.py
+++ Python/basics.py
@@ -423,3 +423,27 @@ my_variable = 5
 while my_variable < 10:
     print("Hello")
     my_variable += 1
+# prints hello 5 times
+
+x = 1
+sum = 0
+while x < 10:
+    sum = sum + x
+    x = x + 1
+
+product = 1
+while x < 10:
+    product = product * x
+    x = x + 1
+
+print(sum, product)
+#Ouput 45 1
+
+def count_down(start_number): 
+    current = start_number
+    while current > 0: 
+        print(current)
+        current -= 1
+    print("Zero!")
+count_down(3)
+#outputs 3, 2, 1, zero!
--- Python/testrun.py
+++ Python/testrun.py
@@ -1,4 +1,7 @@
-my_variable = 5
-while my_variable < 10:
-    print("Hello")
-    my_variable += 1
+def count_down(start_number): 
+    current = start_number
+    while current > 0: 
+        print(current)
+        current -= 1
+    print("Zero!")
+count_down(3)
