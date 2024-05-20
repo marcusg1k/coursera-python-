fcd58c119881894a7af1f9001aeccab4c51e024a
Marcus <marcusgutierrezufc@gmail.com>
2024-05-20 03:08:45 -0500

    basics

--- Python/basics.py
+++ Python/basics.py
@@ -343,4 +343,30 @@ print(exam_grade(65)) # Pass
 print(exam_grade(55)) # Fail
 print(exam_grade(96)) # Top Score
 
+test_num = 12
+if test_num > 15:
+    print(test_num / 4) 
+else: 
+    print(test_num + 3)
+#should print 15 bc 12 is not greater than 15
 
+#only supports a,b,c,d, and returns unknown for all other letters or if letter is uppercase
+def letter_translator(letter): 
+    if letter == "a":
+        letter_position = 1
+    elif letter == "b": 
+        letter_position = 2
+    elif letter == "c":
+        letter_position = 3
+    elif letter == "d": 
+        letter_position = 4
+    else: 
+        letter_position = "unknown" 
+    return letter_position
+    
+print(letter_translator("a"))
+print(letter_translator("b"))
+print(letter_translator("c"))
+print(letter_translator("d"))
+print(letter_translator("e"))
+print(letter_translator("A"))
