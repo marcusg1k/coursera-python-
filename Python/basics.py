01ea39cc0504b1f25481e87451d505c6b23dbae3
Marcus <marcusgutierrezufc@gmail.com>
2024-05-20 13:11:47 -0500

    basics

--- Python/basics.py
+++ Python/basics.py
@@ -520,3 +520,9 @@ multiplication_table(5)
 friends = ['Marcus', 'NPC']
 for friend in friends: 
     print("Hi " + friend)
+
+    product = 1
+for n in range(1, 10): 
+    product = product * n
+print(product)
+#outputs 362880 because 1*2*3*4*5*6*7*8*9 = 362880
