392645eb887bd37063def938b018f85efc1e53ff
Marcus <marcusgutierrezufc@gmail.com>
2024-05-21 13:54:28 -0500

    basics

--- Python/basics.py
+++ Python/basics.py
@@ -563,3 +563,9 @@ for home_team in teams:
   for away_team in teams:
     if home_team != away_team:
       print(home_team + " vs " + away_team)
+      
+      greeting = 'Hello'
+for char in greeting:
+	print(char)
+#outputs H e l l o but in separate lines
+      
---
+++ Python/testrun.py
@@ -0,0 +1,4 @@
+greeting = 'Hello'
+for char in greeting:
+	print(char)
+#outputs H e l l o but in separate lines
