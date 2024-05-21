17e927be6c07e5fae6d4143904a921b20bd0d4e2
Marcus <marcusgutierrezufc@gmail.com>
2024-05-21 15:20:04 -0500

    basics

--- Python/basics.py
+++ Python/basics.py
@@ -602,3 +602,7 @@ def greet_friends(friends):
 
 greet_friends("Marcus")
 #outputs hi + M, hi + A, hi + R, hi + C, hi + U, hi + S
+
+for number in range(1, 6+1, 2):
+    print(number * 3)
+#outputs: 3, 9, 15
--- Python/testrun.py
+++ Python/testrun.py
@@ -1,6 +1,3 @@
-def greet_friends(friends):
-    for friend in friends:
-        print("Hi " + friend)
-
-greet_friends("Marcus")
-#outputs hi + M, hi + A, hi + R, hi + C, hi + U, hi + S
+for number in range(1, 6+1, 2):
+    print(number * 3)
+#outputs: 3, 9, 15
