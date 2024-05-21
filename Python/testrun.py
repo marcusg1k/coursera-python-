0baaa8e6879a01bbea61708e00f9908b97e7fdff
Marcus <marcusgutierrezufc@gmail.com>
2024-05-21 14:11:11 -0500

    basics

--- Python/testrun.py
+++ Python/testrun.py
@@ -1,6 +1,5 @@
-greeting = 'Hello'
-index = 0
-while index < len(greeting):
-	print(greeting[index])
-	index += 1
-	#outputs each letter of the string on a new line
+string1 = "Greetings, Earthlings"
+print(string1[0])   # Prints “G”
+print(string1[4:8]) # Prints “ting”
+print(string1[11:]) # Prints “Earthlings”
+print(string1[:5])  # Prints “Greet”
