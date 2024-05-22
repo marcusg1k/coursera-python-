40b3f4eff120674c4a8b1782c9192b5eb991f679
Marcus <marcusgutierrezufc@gmail.com>
2024-05-22 02:00:11 -0500

    basics

--- Python/basics.py
+++ Python/basics.py
@@ -693,3 +693,10 @@ print(sum)
 for count in range(1, 6): 
     print(count + 1)
     #first output is 2, then 3, 4, 5, 6
+
+    for outer_loop in range(2, 6 + 1): 
+    for inner_loop in range(outer_loop):
+        if inner_loop % 2 == 0: 
+            print(inner_loop)
+
+# the initial value of the outer loop is 2
