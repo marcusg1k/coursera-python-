1475510f51c1fc467b50f04f162b485f87653ec5
Marcus <marcusgutierrezufc@gmail.com>
2024-05-22 15:03:23 -0500

    basics

--- Python/basics.py
+++ Python/basics.py
@@ -790,3 +790,9 @@ message = "A kong string with a silly typo"
 new_message = message[0:2] + "l" + message[3:]
 print(new_message)
 #replaces the 3rd character with an "l"
+
+answer = "YES"
+if answer.lower() == "yes":
+    print("she said yes")
+    #example of using the lower method
+    
--- Python/testrun.py
+++ Python/testrun.py
@@ -1,6 +1,3 @@
-#string slicing
-
-message = "A kong string with a silly typo"
-new_message = message[0:2] + "l" + message[3:]
-print(new_message)
-#replaces the 3rd character with an "l"
+answer = "YES"
+if answer.lower() == "yes":
+    print("User said yes")
