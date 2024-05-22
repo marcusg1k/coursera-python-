a471b41ede1e2190d76a431ee62102845567cd78
Marcus <marcusgutierrezufc@gmail.com>
2024-05-22 15:15:07 -0500

    basics

--- Python/basics.py
+++ Python/basics.py
@@ -795,4 +795,11 @@ answer = "YES"
 if answer.lower() == "yes":
     print("she said yes")
     #example of using the lower method
-    
+
+#modify the student grade function using the format method
+
+def student_grade(name, grade):
+    return "{name} received {grade}% on the exam".format(name=name, grade=grade)
+
+print(student_grade("Marcus", 100))
+print(student_grade("npc", 80))
--- Python/testrun.py
+++ Python/testrun.py
@@ -1,3 +1,7 @@
-answer = "YES"
-if answer.lower() == "yes":
-    print("User said yes")
+#modify the student grade function using the format method
+
+def student_grade(name, grade):
+    return "{name} received {grade}% on the exam".format(name=name, grade=grade)
+
+print(student_grade("Marcus", 100))
+print(student_grade("npc", 80))
