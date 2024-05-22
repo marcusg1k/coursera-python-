ff4bbe1392deae41d06809c2d4a81ed52186bbb9
Marcus <marcusgutierrezufc@gmail.com>
2024-05-22 15:50:26 -0500

    palindrome added to basics

--- Python/basics.py
+++ Python/basics.py
@@ -803,3 +803,22 @@ def student_grade(name, grade):
 
 print(student_grade("Marcus", 100))
 print(student_grade("npc", 80))
+
+#fill in the blanks to complete the is_palindrome function
+# function checks if a string is a palindrome
+
+def is_palindrome(input_string):
+    new_string = "" 
+    reverse_string = ""
+    for letter in input_string:
+        if letter != " ": 
+            new_string = new_string + letter
+            reverse_string = letter + reverse_string
+
+    if new_string.lower() == reverse_string.lower():
+        return True
+    return False
+
+print(is_palindrome("Never Odd or Even")) # should be true
+print(is_palindrome("abc")) # should be false
+print(is_palindrome("kayak")) # should be true
--- Python/testrun.py
+++ Python/testrun.py
@@ -1,1 +1,18 @@
-for c in "abcde": print(c)
+#fill in the blanks to complete the is_palindrome function
+# function checks if a string is a palindrome
+
+def is_palindrome(input_string):
+    new_string = "" 
+    reverse_string = ""
+    for letter in input_string:
+        if letter != " ": 
+            new_string = new_string + letter
+            reverse_string = letter + reverse_string
+
+    if new_string.lower() == reverse_string.lower():
+        return True
+    return False
+
+print(is_palindrome("Never Odd or Even")) # should be true
+print(is_palindrome("abc")) # should be false
+print(is_palindrome("kayak")) # should be true
