77cf9a844339a0311d106366e13ec51570b470b0
Marcus <marcusgutierrezufc@gmail.com>
2024-05-22 23:57:10 -0500

    basics

--- Python/basics.py
+++ Python/basics.py
@@ -930,3 +930,15 @@ def squares(start, end):
 print(squares(2, 3))    # Should print [4, 9]
 print(squares(1, 5))    # Should print [1, 4, 9, 16, 25]
 print(squares(0, 10))   # Should print [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# Convert a list to a tuple
+my_list = [1, 2, 3, 4]
+my_tuple = tuple(my_list)
+
+print(my_tuple)  # Outputs: (1, 2, 3, 4)
+
+# Remember that although parentheses are often used to define a tuple, 
+# they're not always necessary. The following syntax is also valid:
+
+my_tuple = 1, 2, 3, 4
+print(my_tuple)  # Outputs: (1, 2, 3, 4)
