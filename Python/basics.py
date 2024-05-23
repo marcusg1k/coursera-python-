b8acf121e66cc5c52f83ca5996e291f555d50818
Marcus <marcusgutierrezufc@gmail.com>
2024-05-23 00:25:43 -0500

    practice list quiz 100%

--- Python/basics.py
+++ Python/basics.py
@@ -1005,3 +1005,32 @@ def pig_latin(text):
     
 print(pig_latin("hello how are you"))
 print(pig_latin("programming in python is fun"))
+
+def biography_list(people):
+    # Iterate over each "person" in the given "people" list of tuples. 
+    for person in people: 
+
+
+        # Separate the 3 items in each tuple into 3 variables:
+        # "name", "age", and "profession"   
+        name, age, profession = person
+
+
+        # Format the required sentence and place the 3 variables 
+        # in the correct placeholders using the .format() method.
+        print('{} is {} years old and works as {}'.format(name, age, profession))
+
+
+
+
+# Call to the function:
+biography_list([("Ira", 30, "a Chef"), ("Raj", 35, "a Lawyer"), ("Maria", 25, "an Engineer")])
+
+
+# Click Run to submit code
+
+
+# Output should match:
+# Ira is 30 years old and works as a Chef
+# Raj is 35 years old and works as a Lawyer
+# Maria is 25 years old and works as an Engineer
--- Python/testrun.py
+++ Python/testrun.py
@@ -1,12 +1,19 @@
-#create a function that turns text into pig latin
+def biography_list(people): 
+    #iterate over each person in the given people list of tuples
+    for person in people: 
+        #seperate the 3 items in each tuple into 3 variables
+        # name, age, and profession
+        name, age, profession = person
 
-def pig_latin(text): 
-    say = ""
+        #for the required sentence, and place the 3 variables
+        # use .format()
+        print('{} is {} years old and works as a {}'.format(name, age, profession))
 
-    words = text.split()
-    for word in words:
-        say += word[1:] + word[0] + "ay "
-        return say
-    
-print(pig_latin("hello how are you"))
-print(pig_latin("programming in python is fun"))
+
+#call to the function
+biography_list([('Ira', 30, 'chef'), ('Raj', 35, 'Lawyer'), ('Maria', 25, 'an Engineer')])
+
+# output should match
+# Ira is 30 years old and works as a chef
+# Raj is 35 years old and works as a Lawyer
+# Maria is 25 years old and works as an engineer
