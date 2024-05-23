a8b9548aec2d04bd4cb33dd109ad489edb43afdb
Marcus <marcusgutierrezufc@gmail.com>
2024-05-23 00:13:58 -0500

    basics

--- Python/basics.py
+++ Python/basics.py
@@ -978,3 +978,17 @@ for filename in filenames:
         new_filenames.append(filename)
 
 print(new_filenames)
+
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+#gererate new filenames as a list containing the new filenames
+
+new_filenames = []
+for filename in filenames:
+    if filename.endswith(".hpp"):
+        new_filenames.append(filename[:-2])
+    elif filename.endswith(".h"):
+        new_filenames.append(filename[:-1])
+    else:
+        new_filenames.append(filename)
+print(new_filenames)
+#should print ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
