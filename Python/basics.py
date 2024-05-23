97504029824730697add511fff064bd4062dc50a
Marcus <marcusgutierrezufc@gmail.com>
2024-05-22 23:47:58 -0500

    basics

--- Python/basics.py
+++ Python/basics.py
@@ -923,4 +923,10 @@ for x in range(1,11):
     my_list.append(x*2)
 print(my_list)
 
-# Select Run to compare the two results.
+
+def squares(start, end):
+    return [ x**2 for x in range(start, end+1) ]
+
+print(squares(2, 3))    # Should print [4, 9]
+print(squares(1, 5))    # Should print [1, 4, 9, 16, 25]
+print(squares(0, 10))   # Should print [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
--- Python/testrun.py
+++ Python/testrun.py
@@ -1,18 +1,6 @@
-### Simple List Comprehension
-print("List comprehension result:")
+def squares(start, end):
+    return [ x**2 for x in range(start, end+1) ]
 
-# The following list comprehension compacts several lines 
-# of code into one line:
-print([x*2 for x in range(1,11)])
-
-### Long form for loop
-print("Long form code result:")
-
-# The list comprehension above accomplishes the same result as
-# the long form version of the code shown below:
-my_list = []
-for x in range(1,11):
-    my_list.append(x*2)
-print(my_list)
-
-# Select Run to compare the two results.
+print(squares(2, 3))    # Should print [4, 9]
+print(squares(1, 5))    # Should print [1, 4, 9, 16, 25]
+print(squares(0, 10))   # Should print [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
